d4c9af6588429601994bb2ef21668614388269a4
Pis4ni <pis4wrk@gmail.com>
2023-07-25 15:58:20 +0200

    add: snack 1 basic logic

---
+++ js/snack1.js
@@ -0,0 +1,11 @@
+// chiedo all' utente un numero
+let userNumber = 3
+//SE il numero inserito è pari
+if (userNumber % 2 == 0 ) {
+    //stampa il numero inserito
+    consdole.log(userNumber)
+// ALTRIMENTI
+}else{
+    //stampa il numero successivo
+    consdole.log(userNumber+1)
+}
